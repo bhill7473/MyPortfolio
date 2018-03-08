df5e741732a6a3d152f8dde187a85b0ff24f9750
bhill7473 <bhill7473@gmail.com>
2018-03-08 05:13:49 -0500

    commit

---
+++ js/buttons.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  document.getElementById("linkedin").onclick = function linkedin() {
+    return  location.href = "https://www.linkedin.com/in/brent-hillen/";
+  };
+  document.getElementById("github").onclick = function github() {
+      return  location.href = "https://github.com/bhill7473";
+    };
+  document.getElementById("gmail").onclick = function gmail() {
+        return  location.href = "mailto:bhill7473@gmail.com?Subject=Hello%20again";
+      };
+});
---
+++ js/projects.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  document.getElementById("postpick").onclick = function linkedin() {
+    return  location.href = "https://postpick-app.herokuapp.com/";
+  };
+  document.getElementById("excitebite").onclick = function github() {
+      return  location.href = "https://excitesandbites.herokuapp.com/";
+    };
+
+});
