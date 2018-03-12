4e372705fc2b604935520fd584d7197414ce2ece
bhill7473 <bhill7473@gmail.com>
2018-03-12 02:24:59 -0400

    new

--- js/projects.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  document.getElementById("postpick").onclick = function linkedin() {
-    return  location.href = "https://postpick-app.herokuapp.com/";
-  };
-  document.getElementById("excitebite").onclick = function github() {
-      return  location.href = "https://excitesandbites.herokuapp.com/";
-    };
-
-});
