c378c3c3dad158ac762c743593ddfa627c199df9
bhill7473 <bhill7473@gmail.com>
2018-03-27 14:44:36 -0400

    commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+$('#postpick').on('click', function(){
+  location.href = "https://postpick-app.herokuapp.com/";
+
+});
+$('#excitesbites').on('click', function(){
+  location.href = "https://excitesandbites.herokuapp.com/";
+
+});
+$('#mobiledesign').on('click', function(){
+  location.href = "https://mobilewebdesign.herokuapp.com/";
+
+});
+$('#postpick').on('click', function(){
+  location.href = "https://postpick-app.herokuapp.com/";
+
+});
+$('#clothing').on('click', function(){
+  location.href = "https://clothingstore.herokuapp.com/";
+
+});
+$('#btn1').on('click', function(){
+  location.href = "https://www.linkedin.com/in/brent-hillen/";
+
+});
+$('#btn2').on('click', function(){
+  location.href = "https://github.com/bhill7473";
+
+});
+$(document).on('click', 'li', function(){
+  $(this).toggleClass('list');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
